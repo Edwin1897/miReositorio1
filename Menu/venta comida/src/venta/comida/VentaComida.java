0b68f0137285913d306049b593cd1e6139417054
francisco santiago <ed_brok@hotmail.com>
2018-02-15 12:30:17 -0600

    Menu

---
+++ Menu/venta comida/src/venta/comida/VentaComida.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package venta.comida;
+
+/**
+ *
+ * @author 2392
+ */
+public class VentaComida {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
